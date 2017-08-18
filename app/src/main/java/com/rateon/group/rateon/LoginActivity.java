79401019ddd0bc836aa25fcb96d0415a8cf1cb9e
Rishank Kumar <rishank.kumar@gmail.com>
2017-08-18 17:49:20 +0530

    Added intent for signup and login activity

--- app/src/main/java/com/rateon/group/rateon/LoginActivity.java
+++ app/src/main/java/com/rateon/group/rateon/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.rateon.group.rateon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.rateon.group.rateon.R.id.loginBtn;
+import static com.rateon.group.rateon.R.id.signupBtn;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signupBtn = (Button) findViewById(R.id.signupBtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i;
+                i = new Intent(view.getContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
